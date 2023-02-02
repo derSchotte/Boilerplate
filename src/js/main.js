1475eb82e802e90d8da0fa769a7d219f01c93a22
Frank McDonald <kontakt@frank-mcdonald.de>
2023-02-02 10:22:57 +0100

    Finish minimum Boilerplate

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,1 @@
 import './../scss/style.scss';
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      MozDev
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
